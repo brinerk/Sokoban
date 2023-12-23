93b6a9ee6880b30b08ba43971b93134c2d2bc4be
slam <ltmcdejq@gmail.com>
2023-12-23 15:23:35 -0500

    Added smooth movement

--- Scenes/player.cs
+++ Scenes/player.cs
@@ -3,6 +3,10 @@ using System;
 
 public partial class player : Node3D
 {
+
+	private float _t = 0.0f;
+	private Vector3 NewPos = Vector3.Zero;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,13 +15,36 @@ public partial class player : Node3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (Input.IsActionJustPressed("ui_left"))
-				Position = Position - new Vector3(1,0,0);
+
+		_t += (float)delta * 0.4f;
+
+		if (Input.IsActionJustPressed("ui_left")) 
+		{
+			_t = 0.0f;
+			NewPos = Position - new Vector3(1,0,0);
+		}
 		if (Input.IsActionJustPressed("ui_right"))
-				Position = Position + new Vector3(1,0,0);
+		{
+			_t = 0.0f;
+			NewPos = Position + new Vector3(1,0,0);
+		}
 		if (Input.IsActionJustPressed("ui_up"))
-				Position = Position - new Vector3(0,0,1);
+		{
+			_t = 0.0f;
+			NewPos = Position - new Vector3(0,0,1);
+		}
 		if (Input.IsActionJustPressed("ui_down"))
-				Position = Position + new Vector3(0,0,1);
+		{
+			_t = 0.0f;
+			NewPos = Position + new Vector3(0,0,1);
+		}
+
+		Move(NewPos, _t);
+
+	}
+
+	void Move(Vector3 NewPos, float _t)
+	{
+		Position = Position.Lerp(NewPos, _t);
 	}
 }
